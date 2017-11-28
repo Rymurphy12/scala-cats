6db55e7ee6c73ac5c6e4cd0c37a0bb42959afab2
Ryan Murphy <rymurphy12@gmail.com>
2017-11-28 16:24:46 -0600

    First commit of the Scala Cats book exercises

---
+++ src/main/scala/introduction/Json.scala
@@ -0,0 +1,30 @@
+package introduction
+
+sealed trait Json
+final case class JsObject(get: Map[String, Json]) extends Json
+final case class JsString(get: String) extends Json
+final case class JsNumber(get: Double) extends Json
+case object JsNull extends Json
+
+trait JsonWriter[A] {
+  def write(value: A): Json
+}
+
+final case class Person(name: String, email: String)
+
+object JsonWriterInstances {
+  implicit val stringWriter: JsonWriter[String] =
+    new JsonWriter[String] {
+      def write(value: String): Json =
+        JsString(value)
+    }
+
+  implicit val personWriter: JsonWriter[Person] =
+    new JsonWriter[Person] {
+      def write(value: Person): Json =
+        JsObject(Map(
+          "name" -> JsString(value.name),
+          "email" -> JsString(value.email)
+        ))
+    }
+}
