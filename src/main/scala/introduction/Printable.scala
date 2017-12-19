b8ec2a40f27aad043c72f803480827c178c7ba51
rymurphy12 <rymurphy12@gmail.com>
2017-12-19 10:54:21 -0600

    Finshed the exercises in 1.3

--- src/main/scala/introduction/Printable.scala
+++ src/main/scala/introduction/Printable.scala
@@ -21,8 +21,12 @@ object PrintableInstances{
 
   implicit val catPrintable: Printable[Cat] =
     new Printable[Cat] {
-      def format(value: Cat): String =
-        value.name + " is a " + value.age + " year-old " + value.color + " cat."
+      def format(value: Cat): String = {
+        val name = Printable.format(value.name)
+        val age = Printable.format(value.age)
+        val color = Printable.format(value.color)
+        s"$name is a $age year-old $color cat."
+      }
     }
 }
 
@@ -36,3 +40,14 @@ object Printable{
 }
 
 final case class Cat(name: String, age: Int, color: String)
+
+//1.3 Exercise 4
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit p: Printable[A]): String =
+      p.format(value)
+
+    def print(implicit p: Printable[A]): Unit = 
+     println(p.format(value))
+  }
+}
